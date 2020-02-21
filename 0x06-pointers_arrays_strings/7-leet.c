a57a23df761ad6b9f798e527f899b87f88a2d287
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-02-21 15:48:19 -0500

    rot 13, done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+char *leet(char *)
+{
+	int i, j;
+	char letras[] = {a, A, e, E, o, O, t, T, l, L};
+	char num[] = {4, 4, 3, 3, 0, 0, 7, 7, 1 ,1};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0 ; letras[j] != '\0' ; j++)
+		{
+			if(s[i] == num[j])
+		}
+	}
+	i++
+}
+
