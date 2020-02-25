4bb4eda86580f45d4d45b1c8ed45fad12b139deb
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-02-25 17:03:09 -0500

    1-memcpy

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,15 +1,3 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
 int _putchar(char c);
-#endif
-
-#ifndef _MEMSET_H
-#define _MEMSET_H
 char *_memset(char *s, char b, unsigned int n);
-#endif
-
-#ifndef _MEMCPY
-#define _MEMCPY
-char *_memcpy(char *dest, char *src, unsigned int n);
-#endif
-
+char *_memcpy(char *dest, char *src, unsigned int n;
