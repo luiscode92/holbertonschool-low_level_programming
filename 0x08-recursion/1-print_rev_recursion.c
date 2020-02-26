0e8ffe2d4066cab3592fcec20aecb2099f005d19
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-02-26 15:48:49 -0500

    print_rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: input
+ * Return: output
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
