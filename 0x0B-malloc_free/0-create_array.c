e224f37f2d7b1a027ab5b825d71754bb6b5e50d4
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-03-03 18:24:55 -0500

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for ( i = 0 ; i < size ; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
