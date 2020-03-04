47025eaf3c7fd9fed087230e930ebdef04b5aa97
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-03-03 20:48:51 -0500

    2task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0 ; str[i] != '\0' ; i++)
-		a = malloc(i * sizeof(char) + 1);
+		a = malloc(i * sizeof(char));
 	if (a == NULL)
 		return (NULL);
 	for (j = 0 ; j < i ; j++)
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int l = 0, i, j, k, size;
+	char *a;
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{}
+	for (j = 0 ; s2[j] != '\0' ; j++)
+	{}
+	size = i + j + 1;
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (k = 0 ; k < size ; k++)
+	{
+
+		if (k < i)
+		{
+			a[k] = s1[k];
+		}
+		else
+		{
+			a[k] = s2[l++];
+		}
+	}
+	return (a);
+}
