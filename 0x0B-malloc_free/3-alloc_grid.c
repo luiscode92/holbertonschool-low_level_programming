a8ad5f8df8a80facae1e8d97b019572d5fa09125
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-03-04 20:21:17 -0500

    3task update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,6 +13,9 @@ int **alloc_grid(int width, int height)
 	int **grid;
 	int i, j;
 
+	if (width < 1 || height < 1)
+		return (NULL);
+
 	grid = malloc(height * sizeof(*grid));
 	if (grid == NULL)
 		return (NULL);
