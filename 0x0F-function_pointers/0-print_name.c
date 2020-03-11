72ed9b72bcb2b80a4e6f917afa4e4b47c63f9f76
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-03-11 13:42:14 -0500

    first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - function that prints a name.
+ * @name: name of the person
+ * @f: function pointer
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
