144a29a6e896143bdcf8a37ae99bf1ee0c394e65
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-04-01 00:53:03 +0000

    1task upddate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 1000);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (0);
 	if (text_content)
