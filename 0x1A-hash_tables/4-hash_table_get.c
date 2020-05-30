9b8b905bfa6d1ab7d9c8d711bf59c4bab0a57f31
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-30 01:37:10 +0000

    4 hash update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+  *hash_table_get - function that retrieves value associates
+  *@ht: hash table
+  *@key: key
+  *Return: value associated with the element
+  */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char*)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
