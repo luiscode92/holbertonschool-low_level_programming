a37fe590374b646a58fa53468884f4e261c05342
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-30 02:16:03 +0000

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+  *hash_table_delete - funciton that deletes a ht
+  *@ht: hash table
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *tmp = NULL;
+
+	if(!ht)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index])
+		{
+			while (ht->array[index])
+			{
+				tmp = ht->array[index]->next;
+				free(ht->array[index]->key);
+				free(ht->array[index]->value);
+				free(ht->array[index]);
+				ht->array[index] = tmp;
+			}
+			free(ht->array[index]);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
