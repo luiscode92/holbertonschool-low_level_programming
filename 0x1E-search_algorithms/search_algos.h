f7a5c7cf9093f16c647ba6803752d227e5d0cbb5
Luis Sanjuan <luiscode92@gmail.com>
2020-08-27 09:48:55 -0500

    add header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, unsigned int start, unsigned int end);
+int binary_search(int *array, size_t size, int value);
 #endif
